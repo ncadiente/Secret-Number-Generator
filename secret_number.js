145839c02a39840ffaabedf4d72f1db5df5b2db4
Nick Cadiente <nickcadiente@Nicks-MacBook-Pro.local>
2015-12-03 16:31:42 -1000

    passes tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
-'use strict';
+
 module.exports = function() {
-  
+  'use strict';
+  var number = ((Math.random() * 1000000) +1);
+  if (number > 1000000){
+    number=1000000;
+  }
+
+
+  return function() {
+    return number;
+  };
+
 };
